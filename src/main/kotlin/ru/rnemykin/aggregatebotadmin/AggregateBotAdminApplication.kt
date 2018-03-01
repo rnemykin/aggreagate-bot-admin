7f1a1a9d18e411096b5fe39ce77268a7a69d19f4
rnemykin <nemykin.roman@gmail.com>
2018-03-02 00:04:52 +0300

    init

---
+++ src/main/kotlin/ru/rnemykin/aggregatebotadmin/AggregateBotAdminApplication.kt
@@ -0,0 +1,11 @@
+package ru.rnemykin.aggregatebotadmin
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AggregateBotAdminApplication
+
+fun main(args: Array<String>) {
+    runApplication<AggregateBotAdminApplication>(*args)
+}
